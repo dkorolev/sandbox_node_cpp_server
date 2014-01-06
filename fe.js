da5bb18217312cef882001963d51e3379fde2609
Dima Korolev <dmitry.korolev@gmail.com>
2014-01-06 11:43:32 -0800

    Added a static page.

--- fe.js
+++ fe.js
@@ -1,9 +1,13 @@
+var path = require('path');
 var express = require('express');
 
 var app = express();
 
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
 app.get('/', function(req, res) {
-    res.send('OK');
+    res.writeHead(302, { Location: '/static/form.html' });
+    res.end();
 });
 
 app.listen(process.env.PORT || 3000);
